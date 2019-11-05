8b1b40986809d7449b71d2e3393cbbc1d4c85d92
maximlry <maxim.lry@gmail.com>
2019-11-06 00:11:21 +0300

    HelloWorld start

---
+++ src/ru/progwards/maximlry/HelloWorld.java
@@ -0,0 +1,19 @@
+package ru.progwards.maximlry;
+
+public class HelloWorld {
+    static void println(String str) {
+        System.out.println(str);
+    }
+
+    static int addition(int x, int y) {
+        return x + y;
+    }
+
+    public static void main(String[] args) {
+        println("Эй!");
+        println("Привет!!!");
+        System.out.print("Я знаю что 7 + 5 = ");
+        System.out.println(addition(5,7));
+
+    }
+}
