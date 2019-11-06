26132e8d6775a1e1b69ceee7c6246bea2e72e58a
maximlry <maxim.lry@gmail.com>
2019-11-06 23:02:22 +0300

    Revert "HelloWorld start addition1"
    This reverts commit 14e73f40

---
+++ src/ru/progwards/java1/lessons/helloworld/Task1.java
@@ -0,0 +1,13 @@
+package ru.progwards.java1.lessons.helloworld;
+
+public class Task1 {
+    public static void main(String[] args) {
+        String str1 = "Хорошо идут дела";
+        String str2 = "Изучаю Java я!";
+        String str3 = " ";
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str1 + str3 + str2);
+        System.out.println(str2 + str3 + str1);
+    }
+}
---
+++ src/ru/progwards/java1/lessons/helloworld/Task2.java
@@ -0,0 +1,17 @@
+package ru.progwards.java1.lessons.helloworld;
+
+public class Task2 {
+    public static void subtraction(int x, int y) {
+        int z = x - y;
+        System.out.print("Вызвана функция subtraction() с параметрами x = ");
+        System.out.print(x);
+        System.out.print(", y = ");
+        System.out.println(y);
+        System.out.print("Результат z = ");
+        System.out.println(z);
+    }
+
+    public static void main(String[] args) {
+        subtraction(89, 55);
+    }
+}
